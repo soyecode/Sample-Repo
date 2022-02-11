ccb4746ffc4020221ca23ee3c904840acce6bcb5
Olabode Adesoye <olabodeadesoye.gmail.com>
2022-02-11 18:17:12 +0000

    Removed some code

--- Script.py
+++ Script.py
@@ -1,21 +1,6 @@
-import math
-import os
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Olabode'))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
